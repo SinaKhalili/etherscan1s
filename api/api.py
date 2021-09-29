55e69dfc29ec34483c76d130b13b03fd66c05586
Sina Khalili <khalili@sfu.ca>
2021-09-29 00:34:23 -0700

    Add simple getting the contract

--- api/api.py
+++ api/api.py
@@ -4,11 +4,19 @@ This backend will be namespaced like the following:
  - / : This will be the React application, which will have it's own routes
 
 """
+import os
+import json
+
 import time
 from flask import Flask, request
 from web3 import Web3
+import requests
+
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
+
+API_KEY = os.getenv("ETHERSCAN_API_KEY")
+URL = "https://api.etherscan.io/api"
 
 
 @app.route("/")
@@ -20,6 +28,22 @@ def index():
 def get_contract_code():
     args = request.args
     address = args["address"]
-    print(address)
-    print(Web3.isAddress(address))
-    return {"time": time.time()}
+    print(f"fetching {address}")
+    req = requests.get(
+        URL,
+        {
+            "module": "contract",
+            "action": "getsourcecode",
+            "address": address,
+            "apikey": API_KEY,
+        },
+    )
+    ans = req.json()
+    res = ans["result"][0]
+
+    try:
+        files = json.loads(res["SourceCode"][1:-1])
+    except json.JSONDecodeError:
+        files = {"single_file": res["SourceCode"]}
+    # print(Web3.isAddress(address))
+    return files
--- src/pages/CodePage.js
+++ src/pages/CodePage.js
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 
 export const CodePage = () => {
-    const { address } = useParams()
-    const [code, setCode] = useState(0);
+  const { address } = useParams();
+  const [code, setCode] = useState(0);
 
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
 
-    useEffect(() => {
-        console.log(address)
-        fetch('/api/code?address=0x4512347810').then(res => res.json()).then(data => {
-            setCode(data.time);
-        });
-    }, [address]);
+  useEffect(() => {
+    console.log(address);
+    fetch(`/api/code?address=${address}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCode(data["single_file"]);
+      });
+  }, [address]);
 
-    return (
-        <div>
-            <code>
-                {code}
-            </code>
-        </div>
-    );
-}
+  return (
+    <div className="Code">
+      <pre>
+        <code className={`language-javascript`}>{code}</code>
+      </pre>
+    </div>
+  );
+};
